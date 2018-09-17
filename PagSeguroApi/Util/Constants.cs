cb10e963f22a82b678c15fcfbe2fc8b77437f215
Érico Souza <esouza3@csc.com>
2018-09-17 20:11:14 -0300

    Criação das classes utils

---
+++ PagSeguroApi/Util/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace PagSeguroApi.Util
+{
+    public class Constants
+    {
+        public const string BRL = "BRL";
+        public const string CARTAO_CREDITO = "CARTAO_CREDITO";
+        public const string BOLETO = "BOLETO";
+        public const string CREDIT_CARD = "creditCard";
+    }
+}
---
+++ PagSeguroApi/Util/UrlEndpoint.cs
@@ -0,0 +1,12 @@
+﻿namespace PagSeguroApi.Util
+{
+    public class UrlEndpoint
+    {
+        public const string URL_WS_DEV_PAGSEGURO = "https://ws.sandbox.pagseguro.uol.com.br";
+        public const string URL_WS_PROD_PAGSEGURO = "https://ws.pagseguro.uol.com.br";
+
+        public const string PATH_SESSIONS = "/v2/sessions";
+        public const string PATH_PAYMENT = "/v2/checkout/payment.html?code=";
+        public const string PATH_TRANSACTIONS = "/v2/transactions";
+    }
+}
